ab6429ec8e05f587b3bc3c31697fea35698ba89b
radunica66 <mislav.cotic@gmail.com>
2019-04-27 14:02:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return  array;
+}
